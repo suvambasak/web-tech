f3db55942c8d1430e834e3911476f551b8fcf8d7
suvambasak <suvamabsak2008@gmail.com>
2021-03-30 21:20:22 +0530

    week 3 Qus B

--- week_3/B/convert.js
+++ week_3/B/convert.js
@@ -1,20 +1,26 @@
+// Function for validation and calling right function based on unit.
 function convert() {
+    // Taking conversion unit and value.
     var fromUnit = parseInt(document.getElementById("from_select").value);
     var toUnit = parseInt(document.getElementById("to_select").value);
     var inputValue = parseInt(document.getElementById("input_value").value);
 
+    // Log
     console.log(`From ${fromUnit} To ${toUnit}`);
     console.log(`Input value : ${inputValue}`);
 
+    // Validation of unit.
     if (!(fromUnit && toUnit)) {
         alert("Please select the conversion unit!");
         return;
     }
+    // Validation of input value.
     if (isNaN(inputValue)) {
         alert("Input missing!");
         return;
     }
 
+    // Calling function based on input unit.
     switch (fromUnit) {
         case 1:
             kmToOther(toUnit, inputValue);
@@ -34,8 +40,10 @@ function convert() {
 
 }
 
+// Function for convert from kilometer to any other unit.
 function kmToOther(toUnit, inputValue) {
     var output;
+
     switch (toUnit) {
         case 1:
             output = inputValue
@@ -50,12 +58,16 @@ function kmToOther(toUnit, inputValue) {
         default:
             break;
     }
+    // Log.
     console.log(`KM to other : ${toUnit} | value(input):  ${inputValue} | value(output):  ${output}`);
+    // Update the output with converted value
     document.getElementById("output_value").value = output;
 }
 
+// Function for convert from meter to any other unit.
 function mToOther(toUnit, inputValue) {
     var output;
+
     switch (toUnit) {
         case 1:
             output = inputValue / 1000;
@@ -70,12 +82,17 @@ function mToOther(toUnit, inputValue) {
         default:
             break;
     }
+
+    // log.
     console.log(`M to other : ${toUnit} | value(input):  ${inputValue} | value(output):  ${output}`);
+    // Update the output with converted value.
     document.getElementById("output_value").value = output;
 }
 
+// Function for convert from milimeter to any other unit.
 function mmToOther(toUnit, inputValue) {
     var output;
+
     switch (toUnit) {
         case 1:
             output = inputValue / 1000000;
@@ -90,7 +107,10 @@ function mmToOther(toUnit, inputValue) {
         default:
             break;
     }
+
+    // Log.
     console.log(`MM to other : ${toUnit} | value(input):  ${inputValue} | value(output):  ${output}`);
+    // Update the output with converted value.
     document.getElementById("output_value").value = output;
 }
 
